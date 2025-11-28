2614a9990c65dfe5081142a9f626be708af2d596
Ander-17 <anderson.lopera@est.iudigital.edu.co>
2025-11-27 20:47:16 -0500

    All components

---
+++ src/data/creditsData.js
@@ -0,0 +1,79 @@
+export const creditsData = [
+    {
+        id: 1,
+        name: "Crédito de libre inversión",
+        description: "Dinero más rápido",
+        minAmount: 1000000,
+        maxAmount: 3000000,
+        interestRate: 1.8,
+        maxTerm: 60,
+        requirements: "Mayor a 18 años, ingresos mínimos de 1.500.000",
+        icon: "",
+
+    },
+
+    {
+        id: 2,
+        name: "Crédito de libre inversión",
+        description: "Dinero más rápido",
+        minAmount: 1000000,
+        maxAmount: 3000000,
+        interestRate: 1.8,
+        maxTerm: 60,
+        requirements: "Mayor a 18 años, ingresos mínimos de 1.500.000",
+        icon: "",
+
+    },
+
+    {
+        id: 3,
+        name: "Crédito de libre inversión",
+        description: "Dinero más rápido",
+        minAmount: 1000000,
+        maxAmount: 3000000,
+        interestRate: 1.8,
+        maxTerm: 60,
+        requirements: "Mayor a 18 años, ingresos mínimos de 1.500.000",
+        icon: "",
+
+    },
+
+    {
+        id: 4,
+        name: "Crédito de libre inversión",
+        description: "Dinero más rápido",
+        minAmount: 1000000,
+        maxAmount: 3000000,
+        interestRate: 1.8,
+        maxTerm: 60,
+        requirements: "Mayor a 18 años, ingresos mínimos de 1.500.000",
+        icon: "",
+
+    },
+
+    {
+        id: 5,
+        name: "Crédito de libre inversión",
+        description: "Dinero más rápido",
+        minAmount: 1000000,
+        maxAmount: 3000000,
+        interestRate: 1.8,
+        maxTerm: 60,
+        requirements: "Mayor a 18 años, ingresos mínimos de 1.500.000",
+        icon: "",
+
+    },
+
+    {
+        id: 6,
+        name: "Crédito de libre inversión",
+        description: "Dinero más rápido",
+        minAmount: 1000000,
+        maxAmount: 3000000,
+        interestRate: 1.8,
+        maxTerm: 60,
+        requirements: "Mayor a 18 años, ingresos mínimos de 1.500.000",
+        icon: "",
+
+    },
+];
